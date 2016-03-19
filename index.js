eaa3923cb988a7b89f28b143e1954926474b69ba
mdctenga <mdctenga@gmail.com>
2016-03-18 22:57:56 -1000

    Set npm test, nodemon, and initial routes

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/hello-world', function (req, res) {
+app.get('/', function (req, res) {
   res.send('hello world!');
+});
+
+app.get('/yo', function(req, res) {
+  res.send('YO!');
 });
 
 app.listen(3000, function () {
